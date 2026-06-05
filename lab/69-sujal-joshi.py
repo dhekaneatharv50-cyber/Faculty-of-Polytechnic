b1e3073609607632c0f8123435878e8ff11eba66
Sujal joshi <1sujal7j2006@gmail.com>
2026-06-05 09:15:30 +0530

    Update 69-sujal-joshi.py

--- lab/69-sujal-joshi.py
+++ lab/69-sujal-joshi.py
@@ -1,41 +1,51 @@
 #problem 1
-x=0
-total=0
-while x<10:
-    x=x+1
-  total=total+x
-print(total)
-
+i=1
+sum=0
+while i<=10:
+  sum= sum+i
+  i=i+1
+print(sum)
 #problem 2
-def even_odd(number):
-  if number % 2==0:
-    return "Even"
-  else:
-    return"odd"
-a=2
-b=26
-c=37
-print("a",even_odd(a))
-print("b",even_odd(b))
-print("c",even_odd(c))
-
-#Problem 3
-def div_3(number):
-  if number % 3==0:
-    return ("divisible by 3")
-  else:
-    return ("non divisible by 3")
-
 a=4
 b=9
 c=19
-print("a=",div_3(a))
-print("b=",div_3(b))
-print("c=",div_3(c))
-
+def numbers(n):
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+numbers(a)
+numbers(b)
+numbers(c)
+#problem 3
+a=4
+b=9
+c=19
+def numbers (n):
+    if n%3==0:
+        print("divisible by 3")
+    else:
+        print("not divisible")
+numbers(a)
+numbers(b)
+numbers(c)
 #problem 4
 a=[1,2,3]
 def print_list(list):
- for x in list:
-     print(x)
+    for x in list:
+        print(x)
 print_list(a)
+#problem 5
+i=0
+l=[1,2,3,4,5,6]
+l.pop(0)
+l.pop(1)
+l.pop(2)
+print(l)
+#problem 6
+i=0
+l=[1,2,3,4,5,6]
+l.pop(4)
+l.pop(2)
+l.pop(0)
+print(l)
