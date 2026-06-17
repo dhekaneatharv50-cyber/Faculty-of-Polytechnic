7fcf3f2de806ffc278e480ba965662d16c7065d6
dhekaneatharv50-cyber <dhekaneatharv50@gmail.com>
2026-06-17 10:24:35 +0530

    Create solutionCP1.py

---
+++ students/atharv-dhekane /solutionCP1.py
@@ -0,0 +1,30 @@
+#AVG
+def avrage_of_three(m1,m2,m3):
+    return round(m1,m2,m3)/3
+
+#RESULT
+def classify_result(a):
+    if a>=75:
+        return("distinction")
+    elif a>=35:
+        return("pass")
+    else:
+        return("fall")
+
+#GRADE
+def classify_result(a):
+    if a>=90:
+        return("A")
+    elif a>=80:
+        return("B")
+    elif a>=70:
+        return("C")
+    elif a>=60:
+        return("d")
+    elif a<60:
+        return("F")
+
+#FORMAT
+def format_marks(m1,m2,m3):
+    return(f"{m1},{m2},{m3}")
+  
